cccdd862350613652cc692d83e53891f05d07d11
ElMemoGG <za17011262@zapopan.tecmm.edu.mx>
2021-09-09 13:12:45 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[][] Ma = new  int[][]{{651,23},{762,26},{856,30},{1063,34},{1190,43},{1298,48},{1421,52},{1440,57},{1518,58}};
+        OpeRL noc = new OpeRL(Ma);
+
+        int p  = Integer.parseInt(args[0]);
+
+        double b0 = noc.Beta0();
+        double b1 = noc.Beta1();
+
+        System.out.println("b0: "+b0 +" b1: "+ p +" = " + (b0 + b1* p));
+
+    }
+}
---
+++ src/com/company/OpeRL.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class OpeRL {
+
+    private int[][] arr;
+    private float xy, x2, xS, yS;
+
+    public OpeRL(int[][] arr) {
+        this.arr = arr;
+        Calular();
+    }
+    private void Calular(){
+        for (int i = 0; i <arr.length ; i++) {
+            yS += arr[i][0];
+            xy += arr[i][0] * arr[i][1];
+            xS += arr[i][1];
+            x2+= arr[i][1]*arr[i][1];
+        }
+    }
+
+    public float Beta0(){
+        return (x2 * yS- xS * xy)/(arr.length * x2 - xS * xS);
+    }
+    public float Beta1(){return (arr.length * xy - xS *  yS)/(arr.length * x2 - xS * xS);}
+
+
+}
